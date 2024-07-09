639e72ddb234f5452783807afd733091dfbdc6b2
GSHerterton <gsherterton@gmail.com>
2024-07-09 20:08:10 -0300

    salvando

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    //a decision epoch occurs when the vehicle arrives at a location and observers new customer requests.
+    /*the current state sk at decision epoch k includes the time of arrival, the vehicle's current location,
+    passangers on the vehicle, the times at which the passengers were picked up,
+    the pickup and delivery locations of these in-process requests as well as those of any outstanding requests, and the associated time windows.*/
+
+    for(int k = 0; k < 0;)
+
+    return 0;
+}
