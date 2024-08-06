b311fe66fc0e0ae289e94bbc2d1e22e83860e91d
Gabriel Sherterton <gsherterton@gmail.com>
2024-08-05 21:10:46 -0300

    Colocando os arquivos em pastas

--- State.cpp
+++
@@ -1,8 +0,0 @@
-#include "State.h"
-
-State::State(const Config& config){
-    this->decision_epoch = 0;
-    this->time = 0;
-    this->position = 0; //mudar para colocar um depot
-    this->status = vector<int>(config.get_dimension(), 0); //seta todos os status para 0
-}
